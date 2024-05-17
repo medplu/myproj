623b5d2f6d3ce809d1738c504c71a461181461b2
medplu <medpluscollaborate@gmail.com>
2024-05-17 10:35:27 +0300

    Update CategoryCard.jsx

--- frontend/src/components/category/CategoryCard.jsx
+++ frontend/src/components/category/CategoryCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const CategoryCard = ({ icon, title, description }) => {
+const CategoryCard = ({ icon, title, description, onClick }) => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -8,10 +8,15 @@ const CategoryCard = ({ icon, title, description }) => {
   }, []);
 
   return (
-    <div className={`flex flex-col items-center  overflow-x-scroll pb-5 hide-scroll-bar p-2 bg-gray-800 border-orange-500 shadow-2xl rounded-xl transition-all duration-500 ${loaded ? 'opacity-100 transform translate-y-0' : 'opacity-0 -translate-y-2'}`}>
+    <div
+      className={`flex flex-col items-center justify-center w-20 h-20 bg-gray-800 rounded-2xl text-green-600 shadow hover:shadow-md cursor-pointer mb-2 p-1 transition ease-in duration-300 ${
+        loaded ? 'opacity-100 transform translate-y-0' : 'opacity-0 -translate-y-2'
+      }`}
+      onClick={() => onClick(title)}
+    >
       <img src={icon} alt={title} className="w-10 h-10 mb-2 animate-pulse sm:w-20 sm:h-20 md:w-24 md:h-24" />
       <div className="text-center">
-        <h3 className="text-sm font-bold text-white">{title}</h3>
+        <h3 className="text-xs font-bold text-white">{title}</h3>
         <p className="text-gray-700">{description}</p>
       </div>
     </div>
