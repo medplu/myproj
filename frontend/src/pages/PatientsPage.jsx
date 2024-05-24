49b0789a0f92903a9b0fc5291dd32d1be0e32934
medplu <medpluscollaborate@gmail.com>
2024-05-24 15:57:46 +0300

    Update PatientsPage.jsx

--- frontend/src/pages/PatientsPage.jsx
+++ frontend/src/pages/PatientsPage.jsx
@@ -66,7 +66,7 @@ const PatientsPage = () => {
   const loadGoogleMapsScript = () => {
     if (!window.google || !window.google.maps) {
       const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAoaM7PtjK3HDyqcCahZV1G1Q4XOVEz_Rw&callback=renderMap`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
       script.async = true;
       script.defer = true;
       window.renderMap = renderMap;
