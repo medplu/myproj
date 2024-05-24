10456533ea61136f49030a6d83272bd884683d2b
medplu <medpluscollaborate@gmail.com>
2024-05-24 15:48:18 +0300

    Update Hero.jsx

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function DarkVariantCarousel() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -15,7 +16,7 @@ function DarkVariantCarousel() {
   const images = [
     "https://res.cloudinary.com/dws2bgxg4/image/upload/v1714993049/image3_b7pxgc.jpg",
     "https://res.cloudinary.com/dws2bgxg4/image/upload/v1714993048/image2_l4qa43.jpg",
-    "https://res.cloudinary.com/dws2bgxg4/image/upload/v1715100264/cdb1reaaf5jwvcnx2z9c.jpg"
+    "https://res.cloudinary.com/dws2bgxg4/image/upload/v1715661676/l4lremtb1xcbi4m9mwmo.jpg"
   ];
 
   const slides = [
@@ -56,7 +57,7 @@ return (
   >
     {/* Carousel indicators */}
     <div
-      className="absolute inset-x-0 bottom-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+      className="absolute inset-x-0 bottom-0 z-[2] mx-[15%] mb-8 flex list-none justify-center p-0"
       data-twe-carousel-indicators
     >
       {[0, 1, 2].map((index) => (
@@ -90,7 +91,7 @@ return (
             alt={`Slide ${index + 1}`}
           />
           <div className="absolute inset-0 bg-black opacity-50"></div>
-          <div className="absolute inset-x-[15%] bottom-5 py-5 text-center mb-6 text-white md:block">
+          <div className="absolute inset-x-[15%] bottom-5 py-5 text-center mb-20 text-white md:block">
             <h5 className="text-xl">{slides[index].label}</h5>
             <p>{slides[index].content}</p>
           </div>
@@ -99,12 +100,13 @@ return (
     </div>
 
     {/* Carousel controls - prev item */}
+    
     <button
-      className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+      className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center shadow-xl justify-center border-0 bg-none p-0 text-center text-orange-500 opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
       type="button"
       onClick={handlePrev}
     >
-      <span className="inline-block h-8 w-8 dark:grayscale">
+      <span className="h-8 w-8 dark:grayscale">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -121,11 +123,11 @@ return (
 
     {/* Carousel controls - next item */}
     <button
-      className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+      className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center text-pink-50 shadow-lg justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
       type="button"
       onClick={handleNext}
     >
-      <span className="inline-block h-8 w-8 dark:grayscale">
+      <span className=" h-8 w-8 dark:grayscale">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -142,18 +144,20 @@ return (
 
     {/* Buttons */}
     <div className="absolute bottom-0 left-0 right-0 flex justify-between mt-4 px-6 pb-6">
-      <button
+      
+      <Link to='/about'
         className="px-3 py-1 shadow-lg shadow-gray-500/50 bg-blue-600 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97]"
-        onClick={handlePrev}
+      
       >
         Lern More
-      </button>
-      <button
+      </Link>
+      
+      <Link to="/signup" 
         className="px-3 py-1 shadow-lg shadow-gray-500/50 bg-orange-500 text-white rounded-lg text-[15px] cursor-pointer active:scale-[.97]"
-        onClick={handleNext}
+        
       >
         Join us
-      </button>
+      </Link>
     </div>
   </div>
 );
