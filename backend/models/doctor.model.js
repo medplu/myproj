9b0d7ad9d8cf3bc18240416ff3a2e59e77d558ae
medplu <medpluscollaborate@gmail.com>
2024-05-24 15:26:46 +0300

    Update doctor.model.js

--- backend/models/doctor.model.js
+++ backend/models/doctor.model.js
@@ -1,25 +1,52 @@
 import mongoose from 'mongoose';
+
 const doctorsSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required:true,
-    }, 
-    email:{
-        type: String,
-        required:true,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
         unique: true,
-    }, 
-    image:{
-        type:String,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    image: {
+        type: String,
         default: null,
     },
-    isAdmin:{
-        type: Boolean,
-        default: false,
-    }
-   
-   
-    
-}, {timestamps: true})
+    experience: {
+        type: String,
+        default: null,
+    },
+    specialties: {
+        type: String,
+        default: null,
+    },
+    bio: {
+        type: String,
+        default: null,
+    },
+    consultationFee: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+}, { timestamps: true });
 
-export default mongoose.model('doctors', doctorsSchema)
+export default mongoose.model('Doctor', doctorsSchema);
