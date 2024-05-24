3517c473580cdc0ec3511635e6dfda10363e7d69
medplu <medpluscollaborate@gmail.com>
2024-05-24 21:49:36 +0300

    Update PatientsPage.jsx

--- frontend/src/pages/PatientsPage.jsx
+++ frontend/src/pages/PatientsPage.jsx
@@ -64,17 +64,18 @@ const PatientsPage = () => {
   };
 
   const loadGoogleMapsScript = () => {
-    if (!window.google || !window.google.maps) {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
-      script.async = true;
-      script.defer = true;
-      window.renderMap = renderMap;
-      document.head.appendChild(script);
-    } else {
-      renderMap();
-    }
-  };
+  if (!window.google || !window.google.maps) {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    window.initMap = renderMap; // Ensure renderMap is called after the script is loaded
+    document.head.appendChild(script);
+  } else {
+    renderMap();
+  }
+};
+
 
   useEffect(() => {
     if (userLocation) {
