e99faf8be313ed47be5423273c3ec82d14119824
medplu <medpluscollaborate@gmail.com>
2024-05-24 21:47:01 +0300

    Update ServiceSection.jsx

--- frontend/src/components/services/ServiceSection.jsx
+++ frontend/src/components/services/ServiceSection.jsx
@@ -139,17 +139,18 @@ const PatientPage = ({ authUser }) => {
     }
   };
   const loadGoogleMapsScript = () => {
-    if (!window.google || !window.google.maps) {
-      const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
-      script.async = true; // This makes the script load asynchronously
-      script.defer = true;
-      window.initMap = renderMap; // Ensure renderMap is called after the script is loaded
-      document.head.appendChild(script);
-    } else {
-      renderMap();
-    }
-  };
+  if (!window.google || !window.google.maps) {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    window.initMap = renderMap; // Ensure renderMap is called after the script is loaded
+    document.head.appendChild(script);
+  } else {
+    renderMap();
+  }
+};
+
 
   
 
