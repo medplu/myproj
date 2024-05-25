0c354cf72767bb201f7670a7839291e992f63a44
medplu <medpluscollaborate@gmail.com>
2024-05-25 07:29:10 +0300

    Update ServiceSection.jsx

--- frontend/src/components/services/ServiceSection.jsx
+++ frontend/src/components/services/ServiceSection.jsx
@@ -240,7 +240,7 @@ const PatientPage = ({ authUser }) => {
       </section>
 
       
-        <div className="flex justify-center items-center h-50 w-full bg-white rounded-lg shadow-md">
+        <div className="hidden flex justify-center items-center h-50 w-full bg-white rounded-lg shadow-md">
           <div id="map" style={{ height: "400px", width: "100%" }}></div>
         </div>
 
