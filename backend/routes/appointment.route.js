ccca94abcf8ef6d982a239c1540f20f6f9d3d40e
medplu <medpluscollaborate@gmail.com>
2024-07-07 15:40:14 +0300

    Update appointment.route.js

--- backend/routes/appointment.route.js
+++ backend/routes/appointment.route.js
@@ -1,38 +1,35 @@
 import express from 'express';
-import appointmentController from '../controllers/appointment.controller.js';
+import {
+  createAppointment,
+  getAppointments,
+  getNewAppointmentsCount,
+  getUserAppointments, // Import the getUserAppointments function
+  getAllAppointments,
+  resetNewAppointmentsCount,
+  confirmAppointment // Import the confirmAppointment function
+} from '../controllers/appointment.controller.js';
 
-export default function(io) {
-  const router = express.Router();
+const router = express.Router();
 
-  const {
-    createAppointment,
-    getAppointments,
-    getNewAppointmentsCount,
-    getAllAppointments,
-    resetNewAppointmentsCount,
-    confirmAppointment,
-    getUserAppointments
-  } = appointmentController(io);
+// Route to create a new appointment
+router.post('/', createAppointment);
 
-  // Route to create a new appointment
-  router.post('/', createAppointment);
-// Route to book an appointment and update the doctor's schedule
+// Route to get all appointments for a specific doctor
+router.get('/:doctorId', getAppointments);
 
-  // Route to get all appointments for a specific doctor
-  router.get('/:doctorId', getAppointments);
+router.get('/all/:doctorId', getAllAppointments);
 
-  router.get('/all/:doctorId', getAppointments);
+//Route to appointments for a patient 
+router.get('/user/:userId', getUserAppointments);
 
-  router.get('/user/:userId', getUserAppointments);
 
-  // Route to get new appointments count
-  router.get('/new/count/:doctorId', getNewAppointmentsCount);
+// Route to get new appointments count
+router.get('/new/count/:doctorId', getNewAppointmentsCount);
 
-  // Route to reset new appointments count
-  router.post('/new/reset/:doctorId', resetNewAppointmentsCount);
+// Route to reset new appointments count
+router.post('/new/reset/:doctorId', resetNewAppointmentsCount);
 
-  // Route to confirm an appointment
-  router.post('/confirm/:id', confirmAppointment);
+// Route to confirm an appointment
+router.post('/confirm/:id', confirmAppointment); // Add this line
 
-  return router;
-}
+export default router;
