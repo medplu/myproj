835723a0fda9834432c708c6205e18bbbcb4605f
medplu <medpluscollaborate@gmail.com>
2024-09-01 22:56:42 +0300

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,18 @@
 import express from 'express';
-import { logout, login, signup, getMe } from '../controllers/auth.controller.js';
+import { logout, login, signup, getMe, verifyEmail } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.get("/me", protectRoute, getMe);
 
-router.post('/signup',  signup);
+router.post('/signup', signup);
 
-router.post('/login',  login);
+router.post('/login', login);
 
 router.post('/logout', logout);
+
+// Add the new route for email verification
+router.post('/verify', verifyEmail);
 
 export default router;
