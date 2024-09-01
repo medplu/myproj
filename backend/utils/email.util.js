37aa52e645faa2025298aaa79084294a8f13bee3
medplu <medpluscollaborate@gmail.com>
2024-09-01 21:36:24 +0300

    Update email.util.js

--- backend/utils/email.util.js
+++ backend/utils/email.util.js
@@ -1,36 +1,31 @@
 import nodemailer from 'nodemailer';
 
-export const sendVerificationEmail = async (email, token) => {
+export const sendVerificationEmail = async (email, code) => {
     try {
-        // Create a transporter object using the default SMTP transport
         const transporter = nodemailer.createTransport({
-            service: 'gmail', // You can use other services like Outlook, Yahoo, etc.
+            service: 'gmail',
             auth: {
-                user: process.env.AUTH_EMAIL,  // Your email address
-                pass: process.env.AUTH_PASSWORD // Your email password or app-specific password
+                user: process.env.AUTH_EMAIL,
+                pass: process.env.AUTH_PASSWORD
             }
         });
 
-        // Construct the verification link
-        const verificationLink = `${process.env.APP_URL}/verify-email?token=${token}`;
-
-        // Setup email data
         const mailOptions = {
-            from: `"MediPlus Support" <${process.env.AUTH_EMAIL}>`, // Sender address
-            to: email, // List of recipients
-            subject: "Email Verification - MediPlus", // Subject line
+            from: `"MediPlus Support" <${process.env.AUTH_EMAIL}>`,
+            to: email,
+            subject: "Email Verification Code - MediPlus",
             html: `
                 <h3>Hello,</h3>
-                <p>Thank you for registering on MediPlus. Please click the link below to verify your email address:</p>
-                <a href="${verificationLink}">Verify Email</a>
+                <p>Thank you for registering on MediPlus. Please use the following code to verify your email address:</p>
+                <h2>${code}</h2>
+                <p>To complete the verification process, please enter this code on the following page: <a href="${process.env.APP_URL}/verify-email">Verify Email</a></p>
                 <p>If you did not request this, please ignore this email.</p>
                 <br />
                 <p>Best regards,</p>
                 <p>The MediPlus Team</p>
-            ` // HTML body content
+            `
         };
 
-        // Send the email
         await transporter.sendMail(mailOptions);
         console.log(`Verification email sent to ${email}`);
     } catch (error) {
