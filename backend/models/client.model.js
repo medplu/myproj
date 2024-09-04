4976d689bbc3be0e122a355961b17a42dde32aa2
medplu <medpluscollaborate@gmail.com>
2024-09-04 09:30:32 +0300

    Create client.model.js

---
+++ backend/models/client.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const ClientSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Client = mongoose.model('Client', ClientSchema);
+
+export default Client;
