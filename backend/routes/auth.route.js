b02fe2c92815d4084f9e6708387fed511c82b9aa
medplu <medpluscollaborate@gmail.com>
2024-09-08 15:58:15 +0300

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -12,7 +12,7 @@ import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.get("/me", getMe);
+router.get("/me", protectRoute, getMe);
 
 router.post('/signup', signup);
 
